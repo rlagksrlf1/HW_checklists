a1cc797c4af44264ceee105fcf09c0d55f2d66bc
KyungKoo Kang <coush123@gmail.com>
2019-04-10 23:07:17 -0700

    Initial Commit

---
+++ HW_checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_checklists
+//
+//  Created by sgcs on 2019. 4. 10..
+//  Copyright © 2019년 KwangHee Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
