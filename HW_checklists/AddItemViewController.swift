3a870bce04d075d1a7a16f5ec0331ba6f46f46d2
KwangHee Lim <f123412@sogang.ac.kr>
2019-04-13 15:41:39 +0900

    staticTable6

--- HW_checklists/AddItemViewController.swift
+++ HW_checklists/AddItemViewController.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    /* Initial Keyboard */
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
     // MARK:- Actions
     @IBAction func cancel() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+            return nil
     }
 }
